073dae3c238a005526c4300888a8535d431e724a
bejek89 <bejek.rcar@gmail.com>
2021-02-26 16:33:04 +0100

    Zadanie

--- brudnopis.py
+++ brudnopis.py
@@ -1,4 +1,6 @@
-import random
+import random, time
+
+sleep = 2
 
 def zadanie():
     while True:
@@ -15,6 +17,24 @@ def zadanie():
         print('Bardzo dobrze Dawidzie!!!')
     else:
         return (f'No chyba coś ci się pomyliło Dawidzie;) Prawidłowy wynik to {a * b}')
+
+print('Witaj Dawidzie w niesamowitej grze zwanej ...')
+time.sleep(sleep)
+print('TABLICZKĄ MNOŻENIA :D')
+time.sleep(sleep)
+print('aby móc zakończyć gre musisz zdobyć 5 punktów')
+time.sleep(sleep)
+print('za każde dobrze rozwiązane zadanie otrzymujesz jeden punkt')
+time.sleep(sleep)
+print('GOTOWY ?')
+time.sleep(sleep)
+print('ZACZYNAMY !!!')
+print('3')
+time.sleep(1)
+print('2')
+time.sleep(1)
+print('1')
+print()
 
 for i in range(5):
     print(zadanie())
